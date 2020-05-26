3ce6940b77bdabd7f77020c6bcacd969e2035705
Mukunta <42895755+Mukunta@users.noreply.github.com>
2020-05-26 16:00:47 +0200

    Major Design Changes Update
    Implemented dark mode with red color accents. Also deleted unnessesary images from the website.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
 $('#year').text(new Date().getFullYear());
 
+$('body').scrollspy({ target: '#main-nav' });;
+
+// Add Smooth Scrolling
+$('#main-nav a, #home-heading a').on('click', function(e) {
+    if(this.hash !== '') {
+    // Prevent default behaviour
+    e.preventDefault();
+
+    // Store hash 
+    const hash = this.hash;
+
+    // Animate smooth scroll
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 900, function() {
+    // Add hash to URL after scroll
+    window.location.hash = hash;
+    });
+    }
+});
+
 // Ekko Light Box Init
 $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
